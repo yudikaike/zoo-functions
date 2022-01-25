73e746e3d760f2653f3a0c1168bfda21f802dce6
Kaike Yudi <kaikeyudi@outlook.com>
2022-01-25 15:41:37 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
