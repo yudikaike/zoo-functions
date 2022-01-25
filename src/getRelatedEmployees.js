5bd31ac5b153e08987b09b1db5046f642c829406
Kaike Yudi <kaikeyudi@outlook.com>
2022-01-25 17:10:42 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.some((manager) => manager === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const managerList = data.employees.filter((employee) => isManager(employee.id));
+  if (isManager(managerId)) {
+    const reqManager = managerList.find((manager) => manager.id === managerId);
+    const relEmp = data.employees.filter((e) => e.managers.some((man) => man === reqManager.id));
+    return relEmp.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
