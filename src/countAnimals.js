974746b083a2b71d3c671478cc0816a611929269
Kaike Yudi <kaikeyudi@outlook.com>
2022-01-25 19:02:01 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const { specie: name, sex } = animal;
+  const reqSpecie = data.species.find((specie) => specie.name === name);
+  if (sex) {
+    return reqSpecie.residents.reduce((acc, curr) => (curr.sex === sex ? acc + 1 : acc), 0);
+  }
+  return reqSpecie.residents.length;
 }
+
+console.log(countAnimals({ specie: 'penguins', sex: 'male' }));
 
 module.exports = countAnimals;
