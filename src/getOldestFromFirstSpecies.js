e5d040fa85bf8199ca7ec8fd55ec8b492197d4d4
Kaike Yudi <kaikeyudi@outlook.com>
2022-01-26 13:30:05 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployee = data.employees.find((employee) => employee.id === id);
+  const getFirstSpecieId = getEmployee.responsibleFor[0];
+  const findSpecieById = data.species.find((specie) => specie.id === getFirstSpecieId);
+  const oldestAge = findSpecieById.residents.reduce((acc, curr) => {
+    if (curr.age > acc) {
+      return curr.age;
+    }
+    return acc;
+  }, 0);
+  const oldestAnimal = findSpecieById.residents.find((res) => res.age === oldestAge);
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
